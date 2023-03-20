59440efca7d9b64594abbce807eca1ef9ec0f4d8
Sam Corella <samcorella77@gmail.com>
2023-03-20 19:33:39 -0400

    Add route for deleting tasks

--- todo_flask.py
+++ todo_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 tasks = []
@@ -10,3 +10,10 @@ def todo():
         tasks.append(task)
     return render_template('index.html', tasks = tasks)
 
+@app.route('/delete/<int:index>')
+def delete(index):
+    tasks.pop(index)
+    return redirect(url_for('todo'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
