ea0979fb571dfa46f6655aefd1d9a20e57c942e5
Sam Corella <samcorella77@gmail.com>
2023-04-11 18:02:28 -0400

    Create and style dropdown button for task details

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+// Open respective panel when dropdown button is clicked
+
+// const tasks = document.getElementsByClassName('task');
+
+// for (i = 0; i < tasks.length; i++) {
+//   const dropdownBtn = tasks[i].firstElementChild;
+// 	dropdownBtn.addEventListener("click", function() {
+//     const panel = tasks[i].nextElementSibling;
+
+// 		if (panel.style.display === "block") {
+//       panel.style.display = "none";
+// 		} else {
+//       panel.style.display = "block";
+// 		}
+//   });
+// }
